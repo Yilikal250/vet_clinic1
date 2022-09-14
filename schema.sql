60cb377443c93b5ebab164a2f21208f87d93d59e
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-09-14 21:10:19 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD COLUMN species TEXT;
 
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+ALTER TABLE animals 
+DROP COLUMN id, DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN id SERIAL PRIMARY KEY, ADD COLUMN species_id INT;
+
+ALTER TABLE animals 
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
